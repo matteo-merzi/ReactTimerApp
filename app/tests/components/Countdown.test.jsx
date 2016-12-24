5882d7c966ced1f69f841d55647db9fc7f31e98c
Matteo Merzi <matteo.merzi@gmail.com>
2016-12-24 18:07:46 +0100

    Finalize tests & add page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -15,7 +15,7 @@ describe('Countdown', () => {
         it('should set state to started and countdown', (done) => {
             var countdown = TestUtils.renderIntoDocument(<Countdown/>);
             countdown.handleSetCountDown(10);
-            
+
             expect(countdown.state.count).toBe(10);
             expect(countdown.state.countdownStatus).toBe('started');
 
@@ -33,6 +33,30 @@ describe('Countdown', () => {
                 expect(countdown.state.count).toBe(0);
                 done();
             }, 3001);
+        });
+
+        it('should pause countdown on pause status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountDown(3);
+            countdown.handleStatusChange('paused');
+
+            setTimeout( () => {
+                expect(countdown.state.count).toBe(3);
+                expect(countdown.state.countdownStatus).toBe('paused');
+                done();
+            }, 1001);
+        });
+
+        it('should stop reset count on stopped', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountDown(3);
+            countdown.handleStatusChange('stopped');
+
+            setTimeout( () => {
+                expect(countdown.state.count).toBe(0);
+                expect(countdown.state.countdownStatus).toBe('stopped');
+                done();
+            }, 1001);
         });
     });
 });
